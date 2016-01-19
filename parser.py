820afdff058db2809f2d3d298e52bddf7fe25d62
Raian <pranto157@gmail.com>
2016-01-19 16:50:46 +0600

    Added RCGPParser to parse rcgp website

--- parser.py
+++ parser.py
@@ -24,3 +24,24 @@ class BaseParser(object):
 
     def tracker(self):
         raise NotImplemented
+
+
+class RCGPParser(BaseParser):
+
+    def __init__(self):
+        self.key_node = '//div[@class="search-result"]'
+        logger = 'RCGP'
+        self.url = 'http://www.rcgp.org.uk/learning/events-search-results.aspx?CurrentPage=%r'
+        super(RCGPParser, self).__init__(logger)
+
+    def is_parse_continue(self, content, count):
+        try:
+            response = self.parse_node(content, '//div[@class="pagination centered"]')[0]
+        except KeyError:
+            return False
+        else:
+            current_page = int(response.find('ul/li[@class="active"]').text)
+            if current_page != count:
+                return False
+
+        return True
