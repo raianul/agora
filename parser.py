d0a0c9e1046a9c294966017cb25e5dea017302ed
Raian <pranto157@gmail.com>
2016-01-19 16:58:51 +0600

    Added content tracker method for BSGParser Parser

--- parser.py
+++ parser.py
@@ -80,3 +80,18 @@ class BSGParser(BaseParser):
         logger = 'BSGP'
         self.url = 'http://www.bsg.org.uk/events/index.html'
         super(BSGParser, self).__init__(logger)
+
+    def tracker(self):
+        data = []
+        response = self.make_call(call_type='post', url=self.url, data={'limit': 0})
+        parse_html = self.parse_html(response.content)
+
+        for resp in self.parse_node(parse_html, self.key_node):
+            data.append({
+                'date': resp.find('td[@headers="el_date"]/strong').text,
+                'title': resp.find('td[@headers="el_title"]/a').text,
+                'location': resp.find('td[@headers="el_location"]/a').text,
+                'city': resp.find('td[@headers="el_city"]').text,
+                'type': resp.find('td[@headers="el_category"]/a').text,
+            })
+        return data
